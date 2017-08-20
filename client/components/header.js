373d32f98643a7c74bb310bee6597341e2c8ec97
drklrd <srvbhattarai@gmail.com>
2017-08-20 22:02:24 +0545

    add header stylings

--- client/components/header.js
+++ client/components/header.js
@@ -1,16 +1,40 @@
 import React, { Component } from 'react';
 import { graphql } from 'react-apollo';
 import { CurrentUser } from '../queries/queries';
-
+import { Link } from 'react-router';
 
 class Header extends Component{
-    render(){
-        console.log('&&&',this.props.data);
-        return(
-            <div>
-                Header
-            </div>
 
+    renderButtons(){
+
+        const { loading, user } = this.props.data;
+
+        if(loading) { return <div></div>; }
+
+        if(user){
+            return <div>Logout</div>
+        }else{
+            return (
+                <div>
+                    <li> <Link to="/signup" > Sign Up </Link> </li>
+                    <li> <Link to="/login" > Login </Link> </li>
+                </div>
+            );
+        }
+    }
+
+    render(){
+        return(
+            <nav>
+                <div className="nav-wrapper">
+                    <Link to="/" className="brand-logo left">
+                        Home
+                    </Link>
+                    <ul className="right">
+                        {this.renderButtons()}
+                    </ul>
+                </div>
+            </nav>
         );
     }
 }
