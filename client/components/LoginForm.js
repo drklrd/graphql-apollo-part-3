0fd6271f268576d0ce7c13139a56c36d6e22837d
drklrd <srvbhattarai@gmail.com>
2017-08-21 21:54:30 +0545

    add error validation for login

--- client/components/LoginForm.js
+++ client/components/LoginForm.js
@@ -5,6 +5,14 @@ import { graphql } from 'react-apollo';
 
 class LoginForm extends Component{
 
+    constructor(props){
+        super(props);
+
+        this.state = {
+            errors : []
+        };
+    }
+
     onSubmit({ email, password }){
         this.props.mutate({
             variables : { email, password},
@@ -12,13 +20,19 @@ class LoginForm extends Component{
                 query : CurrentUser
             }]
         })
+        .catch(err => {
+            const errors = err.graphQLErrors.map(error => error.message);
+            this.setState({
+                errors
+            });
+        });
     }
 
     render(){
         return(
             <div>
                 <h3>Login</h3>
-                <AuthForm onSubmit={this.onSubmit.bind(this)} />
+                <AuthForm errors={this.state.errors} onSubmit={this.onSubmit.bind(this)} />
             </div>
 
         );
